391590c8efbe726c79b95c8db24f81e667c3f20b
jadechu1 <jade.chu@gmail.com>
2017-01-25 15:57:41 -0500

    git issues

--- src/com/conways/game/GameOfLife.java
+++ src/com/conways/game/GameOfLife.java
@@ -9,7 +9,6 @@ package com.conways.game;
  Any live cell with two or three live neighbours lives on to the next generation.
  Any live cell with more than three live neighbours dies, as if by overpopulation.
  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
  */
 
 public class GameOfLife {
